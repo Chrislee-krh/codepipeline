eebcb728c526c8f3c273aded82185ada94d85017
Chrislee-krh <chrislee.krh@gmail.com>
2024-01-24 17:18:11 +0900

    last

--- src/main/java/com/example/codepipeline/HelloController.java
+++ src/main/java/com/example/codepipeline/HelloController.java
@@ -18,5 +18,5 @@ public class HelloController {
 //    }
 
     @GetMapping("/")
-    public String root() { return "Hi, This page is not modified yet, but this is a revised version.";}
+    public String root() { return "Hi, This page is not modified yet, but this is going to be the last revision.";}
 }
